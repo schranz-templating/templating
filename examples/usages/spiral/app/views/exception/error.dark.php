54295cba97fbdd326272c37423ab3e3b8c1f5de5
Alexander Schranz <alexander@sulu.io>
2022-11-14 22:11:14 +0100

    Refractor Spiral View Integration using ViewManager (#60)

--- examples/usages/spiral/app/views/exception/error.dark.php
+++ examples/usages/spiral/app/views/exception/error.dark.php
@@ -6,8 +6,8 @@
 </stack:push>
 
 <define:body>
-    <div class="wrapper">
-        <div class="placeholder">
+    <div>
+        <div class="placeholder" style="margin: 0 auto; width: fit-content; margin-bottom: 20px;">
             <img src="/images/logo.svg" alt="Framework Logotype" width="200px"/>
             <h2>[[Something went wrong]]. [[Error code]]: {{ $code }}</h2>
 
@@ -17,5 +17,18 @@
                 [[This view file is located in]] <b>app/views/exception/error.dark.php</b>.
             </div>
         </div>
+
+        @if ($exception && env('DEBUG') === true)
+            <div style="display: flex; flex-direction: column; gap: 20px;">
+                @while($exception)
+                    <section style="box-sizing: content-box; max-width: 100%; overflow: auto; background: #efefef; padding: 5px 20px;">
+                        <h3>{{ $exception->getMessage() }}</h3>
+                        <pre>{{ $exception->getTraceAsString() }}</pre>
+                    </section>
+
+                    @php $exception = $exception->getPrevious(); @endphp
+                @endwhile
+            </div>
+        @endif
     </div>
 </define:body>
